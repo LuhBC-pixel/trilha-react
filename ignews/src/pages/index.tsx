ac3ce0d6ea179181d582d174ab75a4b8a38e2cb6
LuisaBC-pixel <bc.luisa@yahoo.com.br>
2022-02-15 11:19:28 -0300

    install stripe and using api

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,10 +1,19 @@
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
 import styles from './Home.module.scss';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -20,9 +29,9 @@ export default function Home() {
           </h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={(product.priceId)} />
         </section>
 
         <img src='/images/avatar.svg' alt='Girl coding' />
@@ -30,3 +39,21 @@ export default function Home() {
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = stripe.prices.retrieve('price_1KTRDNIf8LB1KYsVgZqbBVEA');
+
+  const product = {
+    priceId: (await price).id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format((await price).unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+  };
+};
