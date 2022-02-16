c58d83397ea304031e42e331b811cdfb912cd3e1
LuisaBC-pixel <bc.luisa@yahoo.com.br>
2022-02-16 15:02:06 -0300

    connect with faunadb

--- ignews/README.md
+++ ignews/README.md
@@ -2,6 +2,9 @@ Aula do ignite trilha react
 
 Tecnologias usadas:
 - Next.js
+- TypeScript
+- GitHub Auth
+- Next Auth
 - Stripe
 - FaunaDB
 - Prismic CMS
--- ignews/src/components/SignInButton/index.tsx
+++ ignews/src/components/SignInButton/index.tsx
@@ -8,7 +8,11 @@ export function SignInButton() {
   const { data: session } = useSession();
 
   return session ? (
-    <button className={styles.signInButton} type='button' onClick={() => signOut()}>
+    <button
+      className={styles.signInButton}
+      type='button'
+      onClick={() => signOut()}
+    >
       <FaGithub color='#04d361' />
       {session.user.name}
       <FiX color='#737380' className={styles.closeIcon} />
